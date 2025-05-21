5645bd28562d562ff74526feedc0c8b00b50a95e
Daniel Martin Ojeda <danielm3@student.42madrid.com>
2025-05-21 11:45:44 +0200

    planning and more planning

--- fractol/test03.c
+++ fractol/test03.c
@@ -6,7 +6,7 @@
 /*   By: danielm3 <danielm3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 14:41:55 by danielm3          #+#    #+#             */
-/*   Updated: 2025/05/20 17:51:46 by danielm3         ###   ########.fr       */
+/*   Updated: 2025/05/21 11:27:30 by danielm3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,9 @@ int	main(void)
 	win_ptr= mlx_new_window(mlx_ptr, width, height, "Cat");
 	img_ptr = mlx_new_image(mlx_ptr, width, height);
 	data_address = mlx_get_data_addr(img_ptr, &bpp, &line_length, &endian);
+	printf("bpp: %i\n", bpp);
+	printf("line_length: %i\n", line_length);
+	printf("endian: %i\n", endian);
 	put_pixel(data_address, 10, 10, 0xFF0000, line_length, bpp);
 	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0);
 	mlx_loop(mlx_ptr);
@@ -58,6 +61,6 @@ void	put_pixel(char *addr, int x, int y, int colour, int line_len, int bpp)
 	/* orden: blue, green, red, alpha */
 	addr[offset + 0] = 0;    // blue
 	addr[offset + 1] = 0;    // green
-	addr[offset + 2] = 255;  // red
+	addr[offset + 2] = (char)0xFF;  // red
 	addr[offset + 3] = 0;    // alpha (0 si no usas transparencia)
 }
