f7cb8a50c3a121becae0b5b8453cdeedc731a3a3
Daniel Martin Ojeda <danielm3@student.42madrid.com>
2025-05-22 12:11:32 +0200

    progress

---
+++ fractol/libft/ft_atof.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danielm3 <danielm3@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 10:14:46 by danielm3          #+#    #+#             */
+/*   Updated: 2024/12/21 23:25:19 by danielm3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(char c)
+{
+	return (c == ' ' || c == '\n' || c == '\f'
+		|| c == '\r' || c == '\t' || c == '\v');
+}
+
+
+
+double	ft_atof(const char *str)
+{
+	int	i;
+	int	mincount;
+	int	result;
+
+	i = 0;
+	result = 0;
+	mincount = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	if (!ft_isdigit(str[i]))
+		return (0);
+	while (ft_isdigit(str[i]))
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	if (str[i] != '\0')
+		return (0);
+	return (result * sign);
+}
---
+++ fractol/main.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danielm3 <danielm3@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 10:20:30 by danielm3          #+#    #+#             */
+/*   Updated: 2025/05/22 11:32:11 by danielm3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	print_wrong_args()
+{
+	char	*error_msg;
+	char	*tmp_join;
+	
+	error_msg = ft_strdup("Incorrect arguments. Please enter:\n");
+	tmp_join = ft_strjoin(error_msg, "Option 1: mandlebrot.\n");
+	free(error_msg);
+	error_msg = ft_strjoin(tmp_join, "Option 2: julia <real part>"
+		"<imaginary part>\nNumber range: -2 ~ +2\n");
+	free(tmp_join);
+	ft_putendl_fd(error_msg, 2);
+	free(error_msg);
+	exit(EXIT_FAILURE);
+}
+
+void	check_args(int argc, char **argv)
+{
+	double	real;
+	double 	imaginary;
+	
+	real = 0;
+	imaginary = 0;
+	if ((ft_strncmp(argv[1], "mandlebrot", 10) != 0 && argc != 2)
+		|| (ft_strncmp(argv[1], "julia", 5) != 0 && (argc != 4 && argc != 2)))
+		print_wrong_args();
+	if (ft_atof(argv[2]) > 2.0 || ft_atof(argv[2]) < -2.0)
+		print_wrong_args();
+		
+}
+
+int	main(int argc, char **argv)
+{
+	check_args(argc, argv);
+	
+	return (0);
+}
+
