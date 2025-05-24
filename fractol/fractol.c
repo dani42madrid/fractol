91d360ef2a96315ee85b4a52c8f94ea6e499a1b5
danielm3 <danielm3@student.42madrid.com>
2025-05-24 18:44:24 +0200

    so many changes

--- fractol/fractol.c
+++ fractol/fractol.c
@@ -6,7 +6,7 @@
 /*   By: danielm3 <danielm3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 17:03:31 by danielm3          #+#    #+#             */
-/*   Updated: 2025/05/24 17:39:43 by danielm3         ###   ########.fr       */
+/*   Updated: 2025/05/24 18:38:42 by danielm3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,8 @@ int	escape_iterations(double zr, double zi, t_map *map)
 
 void	mlx_and_img_creation(t_mlx *mlx)
 {
+	mlx->width = 1280;
+	mlx->height = 1280;
 	mlx->mlx_ptr = mlx_init();
 	mlx->win_ptr = mlx_new_window(mlx->mlx_ptr, mlx->width, mlx->height,
 			"fractol");
--- fractol/main.c
+++ fractol/main.c
@@ -6,7 +6,7 @@
 /*   By: danielm3 <danielm3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 10:20:30 by danielm3          #+#    #+#             */
-/*   Updated: 2025/05/24 18:29:21 by danielm3         ###   ########.fr       */
+/*   Updated: 2025/05/24 18:35:11 by danielm3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main(int argc, char **argv)
 {
-	t_mlx	*mlx;
-	t_map	*map;
+	t_mlx	mlx;
+	t_map	map;
 
 	check_args(argc, argv, &map);
 	mlx_and_img_creation(&mlx);
