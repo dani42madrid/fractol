6657abbbc27e8c20155a98429548f66732182f5b
danielm3 <danielm3@student.42madrid.com>
2025-05-28 13:14:56 +0200

    leaks free

--- fractol/event_handling_2.c
+++ fractol/event_handling_2.c
@@ -6,7 +6,7 @@
 /*   By: danielm3 <danielm3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 23:00:26 by danielm3          #+#    #+#             */
-/*   Updated: 2025/05/27 23:15:10 by danielm3         ###   ########.fr       */
+/*   Updated: 2025/05/28 13:11:37 by danielm3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	on_key_press(int keycode, void *param)
 	{
 		mlx_destroy_image(env->mlx->mlx_ptr, env->mlx->img_ptr);
 		mlx_destroy_window(env->mlx->mlx_ptr, env->mlx->win_ptr);
+		mlx_destroy_display(env->mlx->mlx_ptr);
+		free(env->mlx->mlx_ptr);
 		exit(0);
 	}
 	if (keycode == XK_Left || keycode == XK_Right)
